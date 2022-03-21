9fe5d1d0d0565ba17774047f1ab516c65b252664
Keven Molina <kevenmolina@gmail.com>
2022-03-20 21:03:04 -0400

    Image Selection Created

--- main.js
+++ main.js
@@ -11,13 +11,28 @@ let galleryImages = [];
 for (let images = 1; images <= amountOfImagesInGallery; images++) {
     galleryImages.push(`images/pic${images}.jpg`)
 }
-console.log(galleryImages)
 // Load images to NavBar
-for (let images = 0; images <= amountOfImagesInGallery; images++) {
+for (let images = 0; images < amountOfImagesInGallery; images++) {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', galleryImages[images]);
     newImage.setAttribute('size', 'auto');
+    newImage.addEventListener('click',imageSelect);
     thumbBar.appendChild(newImage);
 }
 
+// Users should be able to select an image from the navbar
+let imageSelection = thumbBar.children;
+//  imageSelection.array.forEach(element => {
+     
+//  });
+
+ function imageSelect(e){
+    //  Parse String
+    //  http://127.0.0.1:5500/images/pic1.jpg
+    // Remove the first 22 characters
+    let parsedImageValue = (e.target.src).slice(22)
+    return displayedImage.setAttribute('src',parsedImageValue)
+ }
+
+//  imageSelection[0].addEventListener('click',imageSelect)
 /* Wiring up the Darken/Lighten button */
